2a459b34618c2ad91ec84b1e9846d74465bb1e64
Bartek <bartosz.piotr.baranowski@gmail.com>
2020-11-15 19:27:52 +0100

    models

--- proticket/API/admin.py
+++ proticket/API/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    fields=['id','date']
--- proticket/API/models.py
+++ proticket/API/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+
+    id = models.AutoField(primary_key=True, editable=False)
+    date = models.DateTimeField()
+
+
+
+class Ticket(models.Model):
+
+    TICKET_TYPE_CHOICES = [
+        ("REGULAR", 'regular'),
+        ("BUDGET", 'budget'),
+        ("PREMIUM", 'premium'),
+        ("VIP", 'vip')]
+
+    PAYMENT_STATUS_CHOICES = [
+        ("NONE", 'none'),
+        ("STARTED", 'started'),
+        ("PAYED", 'payed'),
+        ("ERROR", 'error')]
+
+       
+    event = models.ForeignKey(Event,  on_delete=models.CASCADE)
+    type = models.CharField(max_length=16, choices=TICKET_TYPE_CHOICES)
+    price= models.DecimalField(max_digits=10, decimal_places=2)
+    sold_status = models.BooleanField(default=False)
+    reserved = models.BooleanField(default = False)
+    payment_status = models.CharField(choices =PAYMENT_STATUS_CHOICES, max_length=16, default='none')
+    reserved_until = models.DateTimeField()
